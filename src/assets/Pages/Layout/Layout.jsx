525bb9b7adf5f88a002bba511f21586f6a459955
sayed260 <saiedmosaied345@gmail.com>
2026-03-11 00:00:40 +0200

    fix navbar path

--- src/assets/Pages/Layout/Layout.jsx
+++ src/assets/Pages/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../../Components/Navbar/navbar'
+import Navbar from '../../Components/Navbar/Navbar'
 import Footer from '../../Components/Footer/Fooder'
 import { Outlet } from 'react-router-dom'
 
